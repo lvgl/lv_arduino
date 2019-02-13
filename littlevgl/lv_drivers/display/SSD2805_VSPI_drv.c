08268c62996698fedcc8a063c33dddd6886db64b
techtoys <johnleung00@gmail.com>
2019-02-13 19:46:51 +0800

    Add files via upload

---
+++ littlevgl/lv_drivers/display/SSD2805_VSPI_drv.c
@@ -0,0 +1,384 @@
+#include "SSD2805_VSPI_drv.h"
+
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
+/**
+ * @brief SPI pre transmission callback function to set DC pin for command(0) or data(1)
+ */ 
+static void spiPreTxCallback(spi_transaction_t *t)
+{
+  int dc = (int)t->user;
+  gpio_set_level((gpio_num_t)PIN_NUM_SDC, dc);
+}
+
+static inline void setDC(bool v){(v==1)?(gpio_set_level((gpio_num_t)PIN_NUM_SDC,1)):(gpio_set_level((gpio_num_t)PIN_NUM_SDC,0));}
+static inline void setCS(bool v){(v==1)?(gpio_set_level((gpio_num_t)PIN_NUM_CS,1)):(gpio_set_level((gpio_num_t)PIN_NUM_CS,0));}
+static void setRST(bool v){(v==1)?(gpio_set_level((gpio_num_t)PIN_NUM_RST,1)):(gpio_set_level((gpio_num_t)PIN_NUM_RST,0));}
+    
+static void writeRegister(uint8_t reg, const uint8_t *data8, uint32_t len);
+static void writeCmd(uint8_t cmd);
+static void setTDCSize(uint32_t byteCount);
+static bool setRectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+
+static spi_device_handle_t ssd2805;
+static spi_transaction_t trans[MAX_TRANS_NUM];
+
+/**
+ * @brief Like conventional Arduino driver, this begin() method should be called before using D2805
+ */
+void SSD2805_begin(void)
+{
+    esp_err_t err;
+    spi_bus_config_t  bus_config;
+    spi_host_device_t host;
+    spi_device_interface_config_t dev_config;
+    
+    gpio_set_direction((gpio_num_t)PIN_NUM_SDC, (gpio_mode_t)GPIO_MODE_OUTPUT);
+    gpio_set_direction((gpio_num_t)PIN_NUM_RST, (gpio_mode_t)GPIO_MODE_OUTPUT);
+
+    SSD2805_hwReset();
+
+    (SPI_NUM==VSPI)?(host=VSPI_HOST):(host=HSPI_HOST);
+    
+    {
+      bus_config.mosi_io_num = PIN_NUM_MOSI;
+      bus_config.miso_io_num = PIN_NUM_MISO;
+      bus_config.sclk_io_num = PIN_NUM_SCK;
+      bus_config.quadwp_io_num = -1;
+      bus_config.quadhd_io_num = -1;
+      bus_config.max_transfer_sz = MAX_DMA_LEN; // in byte count
+    }
+
+    dev_config.flags = SPI_DEVICE_HALFDUPLEX;
+    dev_config.mode = 0;
+    dev_config.clock_speed_hz = 2000000UL;  //40MHz works with SPI_DEVICE_HALFDUPLEX, useful for speeding up block write after PLL enabled
+    dev_config.spics_io_num = PIN_NUM_CS;   //use GPIO for chip select
+    dev_config.queue_size = MAX_TRANS_NUM;  //only 1 transaction at any time. 
+    dev_config.pre_cb = spiPreTxCallback;   //pre transmission callback to clear/set SDC line for cmd/data
+
+    err=spi_bus_initialize(host, &bus_config, 1);
+    ESP_ERROR_CHECK(err);
+    err = spi_bus_add_device(host, &dev_config, &ssd2805);
+    ESP_ERROR_CHECK(err);
+
+    //Step 1: set PLL
+    uint8_t txbuffer[12];
+    txbuffer[0]=0x0f; txbuffer[1]=0x00;
+
+    writeRegister(0xba, (const uint8_t *)&txbuffer, 2); //PLL 	= clock*MUL/(PDIV*DIV)
+                                                        //		= clock*(BAh[7:0]+1)/((BAh[15:12]+1)*(BAh[11:8]+1))
+                                                        //		= 20*(0x0f+1)/1*1 = 20*16 = 320MHz
+                                                        //Remark: 350MHz >= fvco >= 225MHz for SSD2805 since the max. speed per lane is 350Mbps
+
+    txbuffer[0]=0x01; txbuffer[1]=0x00;
+    writeRegister(0xb9, (const uint8_t *)&txbuffer, 2);       //enable PLL
+    
+    vTaskDelay(10/portTICK_PERIOD_MS);  ;   //delay 10ms to wait for PLL stablize
+
+    //Step 2: Now it is safe to set SPI up to 40MHz (remove it for now with strip wire prototype )
+    //equivalent to calling SPI.setFrequency(40000000UL);   //now we can set SPI clock up to 40MHz (PLL clock=320/8)
+    spi_bus_remove_device(ssd2805); //has to remove device before switching to a faster SPI clock
+    spi_bus_free(host);
+    dev_config.clock_speed_hz = 40000000UL;
+    err=spi_bus_initialize(host, &bus_config, 1); ESP_ERROR_CHECK(err);
+    err = spi_bus_add_device(host, &dev_config, &ssd2805); ESP_ERROR_CHECK(err);
+  
+    //Step 3: set clock control register for SYS_CLK & LP clock speed
+    //SYS_CLK = TX_CLK/(BBh[7:6]+1), TX_CLK = external oscillator clock speed
+    //In this case, SYS_CLK = 20MHz/(1+1)=10MHz. Measure SYS_CLK pin to verify it.
+    //LP clock = PLL/(8*(BBh[5:0]+1)) = 320/(8*(4+1)) = 8MHz, conform to AUO panel's spec, default LP = 8Mbps
+    //S6D04D2 is the controller of AUO 1.54" panel.
+    txbuffer[0]=0x44; txbuffer[1]=0x00;
+    writeRegister(0xbb, (const uint8_t *)&txbuffer, 2);
+
+    txbuffer[0]=0x00; txbuffer[1]=0x01;
+    writeRegister(0xd6, (const uint8_t *)&txbuffer, 2);   //output sys_clk for debug. Now check sys_clk pin for 10MHz signal
+
+    //Step 4: Set MIPI packet format
+    txbuffer[0]=0x43; txbuffer[1]=0x02;
+    writeRegister(0xb7, (const uint8_t *)&txbuffer, 2);   //EOT packet enable, write operation, it is a DCS packet
+                                                    //HS clock is disabled, video mode disabled, in HS mode to send data
+    //Step 5: set Virtual Channel (VC) to use
+    txbuffer[0]=0x00; txbuffer[1]=0x00;
+    writeRegister(0xb8, (uint8_t *)&txbuffer, 2);
+
+    //Step 6: Now write DCS command to AUO panel for system power-on upon reset
+    setTDCSize(0);
+    writeRegister(DCS_SLPOUT, NULL, 0);
+
+    vTaskDelay(100/portTICK_PERIOD_MS);					//wait for AUO/LG  panel after sleepout(Important:cannot use delay(100)!)
+
+    //Step 7: Now configuration parameters sent to AUO/LG panel for 16-bits/pixel
+    setTDCSize(1);
+    txbuffer[0] = 0x05;
+    writeRegister(DCS_COLMOD, (const uint8_t *)&txbuffer, 1); //Set 0x3a to 0x05 (16-bit color)
+
+    //Step 8: Set TE line and VBP time
+
+    setTDCSize(2);
+    txbuffer[0]=0x5A; txbuffer[1]=0x5A;
+    writeRegister(0xF1, (const uint8_t *)&txbuffer, 2);  //unlock MCS
+
+    //Set MCS register at 0xF2 to set VBP, VFP, and TE time
+    //Setup here establishes a TE time sync. with VBP = 5.64ms with display time = 14.40ms
+    //leading to a frame rate of 50fps. Calculation can be seen from an Excel sheet
+    //Result verified with a DSO. This is a significant result as we may now sync with a slower mcu for flicker-free display.
+    // *** Add support for TE and VBP time set here ***
+    //Trying larger NVBP and NVFP values for slower framerate with blank time =25.56ms, display time =14.40ms
+ 
+    setTDCSize(12);
+    txbuffer[0]=0x00;     //NL=0x00 (default)
+    txbuffer[1]=0xF0;     //NHW=0xF0 (default for 240 clocks per line)
+    txbuffer[2]=0x03;     //frame inversion etc. leave it default
+    txbuffer[3]=0x56;     //NVBP=0x56 (.86) for a vertical back porch in normal mode
+    txbuffer[4]=0x08;     //NVFP=8 for a vertical front porch in normal mode
+    txbuffer[5]=0x00;     //RSEL[1:0]=00 for 240x240 resolution
+    txbuffer[6]=0x01;     //TE = ON
+    txbuffer[7]=0x00;     //TE_ST set zero for TE signal starts on internal VS
+    txbuffer[8]=0x00;
+    txbuffer[9]=0x00;     //TE_ED set the same value as NVBP to synchronize with NVBP
+    txbuffer[10]=0x56;
+    txbuffer[11]=0x03;    //set TE line as active high
+    writeRegister(0xF2, (const uint8_t *)&txbuffer, 12);  //Set DISPCTL for porch and TE values
+  
+    setTDCSize(2);
+    txbuffer[0]=0xA5; txbuffer[1]=0xA5;
+    writeRegister(0xF1, (const uint8_t *)&txbuffer, 2);  //lock MCS
+
+    SSD2805_clearLCD(0x0000); //clearLCD after DCS_DISPON if TE pin is to sync with MCU write
+
+    setTDCSize(0);
+    writeRegister(DCS_DISPON, NULL, 0);                  //display ON DCS command to AUO panel    
+}
+
+void writeCmd(uint8_t cmd)
+{
+    memset(&trans[0], 0, sizeof(spi_transaction_t));
+    trans[0].length = 8;
+    trans[0].user = (void*)0; //SDC pin low for command
+    trans[0].flags = SPI_TRANS_USE_TXDATA;
+    trans[0].tx_data[0] = cmd;
+    esp_err_t err = spi_device_transmit(ssd2805, &trans[0]); 
+    ESP_ERROR_CHECK(err);
+}
+
+void writeRegister(uint8_t reg, const uint8_t *data8, uint32_t len)
+{
+    memset(&trans[0], 0, sizeof(spi_transaction_t));
+
+    trans[0].length = 8;
+    trans[0].user = (void*)0; //SDC pin low for command
+    trans[0].flags = SPI_TRANS_USE_TXDATA;
+    trans[0].tx_data[0] = reg;
+
+    //setCS(0);   //chip select manually not required because spi_device_transmit will do it automatically
+    esp_err_t err = spi_device_transmit(ssd2805, &trans[0]); 
+    ESP_ERROR_CHECK(err);
+
+    if(len!=0)
+    {
+    trans[0].length = 8*len;
+    trans[0].user = (void*)1; //SDC pin high for data
+    trans[0].flags = 0;
+    trans[0].tx_buffer = data8;
+    err = spi_device_transmit(ssd2805, &trans[0]); 
+    ESP_ERROR_CHECK(err);
+    }
+}
+
+/**
+ * @brief Set packet size sending to mipi display in byte count
+ */ 
+void setTDCSize(uint32_t byteCount)
+{
+    uint8_t txbuffer[2];
+    txbuffer[0] = (uint8_t)byteCount;
+    txbuffer[1] = (uint8_t)(byteCount>>8);
+
+    writeRegister(0xbc, (const uint8_t *)&txbuffer, 2); //Set packet size TDC[15:0]
+
+    txbuffer[0] = (uint8_t)(byteCount>>16);
+    txbuffer[1] = (uint8_t)(byteCount>>24);   
+
+    writeRegister(0xbd, (const uint8_t *)&txbuffer, 2); //Set packet size TDC[31:16]
+}
+
+/**
+ * @brief Set the area to draw on LCD
+ * @param (x2,y2) is the top left corner coordinates, (x1,y1) is the lower right corner
+ * @return 0 if failed, 1 if successful
+ */
+bool setRectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
+{
+  if((x1>x2) || (y1>y2)) return 0;
+
+  uint8_t txbuffer[4];
+  
+  uint16_t sc = min(x1, DISP_HOR_SIZE-1);
+  uint16_t sp = min(y1, DISP_VER_SIZE-1);
+  uint16_t ec = min(x2, DISP_HOR_SIZE-1);
+  uint16_t ep = min(y2, DISP_VER_SIZE-1);
+ 
+  setTDCSize(4);
+
+  txbuffer[0] = (uint8_t)(sc>>8);  //SC[15:8]  start column
+  txbuffer[1] = (uint8_t)sc;       //SC[7:0]
+  txbuffer[2] = (uint8_t)(ec>>8);  //EC[15:8] end column
+  txbuffer[3] = (uint8_t)ec;       //EC[7:0] 
+  writeRegister(DCS_COL_SET, (const uint8_t *)&txbuffer, 4);
+
+  txbuffer[0] = (uint8_t)(sp>>8); //SP[15:8]  start page
+  txbuffer[1] = (uint8_t)sp;      //SP[7:0]
+  txbuffer[2] = (uint8_t)(ep>>8); //EP[15:8] end page
+  txbuffer[3] = (uint8_t)ep;      //EP[7:0]
+  writeRegister(DCS_PAGE_SET, (const uint8_t *)&txbuffer, 4);
+
+  return 1;
+}
+
+/**
+ * @brief Toggle reset input for SSD2805
+ */
+void SSD2805_hwReset(void)
+{
+  setRST(0);
+  vTaskDelay(10/portTICK_PERIOD_MS);
+  setRST(1);
+  vTaskDelay(10/portTICK_PERIOD_MS);
+}
+
+/**
+ * @brief Clear the whole display with a color
+ * @param color in 2 bytes per pixel (R4R3R2R1R0G5G4G3 G2G1G0B4B3B2B1B0)
+ */ 
+void SSD2805_clearLCD(uint16_t color)
+{  
+  SSD2805_dispFill(0,0,DISP_HOR_SIZE-1, DISP_VER_SIZE-1, color);
+}
+
+/**
+ * @brief Fill display for a certain area with a color
+ * @param (x2,y2) is the top left corner coordinates, (x1,y1) is the lower right corner 
+ * @param color in 2 bytes per pixel (R4R3R2R1R0G5G4G3 G2G1G0B4B3B2B1B0)
+ */ 
+void SSD2805_dispFill(int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint16_t color)
+{
+  esp_err_t err;
+  
+  if(!setRectangle(x1,y1,x2,y2)) return;
+  
+  uint32_t byteCount2Flush = (x2-x1+1)*(y2-y1+1)*2;
+  //printf("dispFlush::byteCount2Flush = %d\n", byteCount2Flush);
+  setTDCSize(byteCount2Flush);
+  
+  uint32_t dmaBlockCount = 0;
+  if(byteCount2Flush >= MAX_DMA_LEN)
+  {
+    dmaBlockCount = byteCount2Flush/MAX_DMA_LEN; 
+    //printf("dispFlush::byteCount2Flush > MAX_DMA_LEN so, dmaBlockCount = %d\n", dmaBlockCount);
+  }
+  
+  byteCount2Flush%=MAX_DMA_LEN;  //byteCount2Flush becomes the remainder
+  writeCmd(DCS_RAMWR);
+
+    uint16_t *vdb = (uint16_t *)heap_caps_malloc(MAX_DMA_LEN, MALLOC_CAP_DMA);
+    assert(vdb!=NULL);
+
+  for(uint32_t i=0; i<MAX_DMA_LEN/2; i++)
+    vdb[i] = color;
+
+  memset(&trans[0], 0, sizeof(spi_transaction_t));
+/*
+  err = gpio_intr_enable((gpio_num_t)PIN_NUM_TE); assert(err==ESP_OK);
+
+  while(!TE_TriggerFlag)
+  ;
+
+  err = gpio_intr_disable((gpio_num_t)PIN_NUM_TE); assert(err==ESP_OK);
+  TE_TriggerFlag = false;
+*/
+  //sync with TE pin pending...
+  while(dmaBlockCount--){
+    trans[0].length = MAX_DMA_LEN*8;
+    trans[0].user = (void*)1; //SDC pin high for data
+    trans[0].tx_buffer = vdb;
+    err = spi_device_transmit(ssd2805, &trans[0]); 
+    ESP_ERROR_CHECK(err);
+  }
+
+  if(byteCount2Flush){
+    trans[0].length = byteCount2Flush*8;
+    //trans[0].user = (void*)1; //SDC pin high for data
+    //trans[0].tx_buffer = vdb;
+    err = spi_device_transmit(ssd2805, &trans[0]); 
+    ESP_ERROR_CHECK(err);
+  }
+
+  free(vdb);
+
+}
+
+/**
+ * @brief Fill display for a certain area with data from flash
+ * @param (x2,y2) is the top left corner coordinates, (x1,y1) is the lower right corner
+ * @param *color_p is a pointer to data from Flash
+ * @note  SSD2805 configure in 64k color
+ *        1st write G2G1G0B4B3B2B1B0  green_l:3 blue:5
+ *        2nd write R4R3R2R1R0G5G4G3  red:5 green_h:3
+ *        e.g. uint16_t vdb[4] = {0xABCD, 0x1234, 0x5678, 0xaab6};  //color arranged in R4R3R2R1R0G5G4G3 G2G1G0B4B3B2B1B0
+ *        SPI send in 8-bit in this order
+ *        CDh->ABh->34h->12h->78h->56h->B6h->AAh
+ */
+void SSD2805_dispFlush(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const uint16_t* color_p)
+{
+  esp_err_t err;
+
+  if(!setRectangle(x1,y1,x2,y2)) return;
+  
+  uint32_t byteCount2Flush = (x2-x1+1)*(y2-y1+1)*2;
+
+  setTDCSize(byteCount2Flush);
+  
+  uint32_t dmaBlockCount = 0;
+  if(byteCount2Flush >= MAX_DMA_LEN)
+  {
+    dmaBlockCount = byteCount2Flush/MAX_DMA_LEN; 
+  }
+  
+  byteCount2Flush%=MAX_DMA_LEN;  //byteCount2Flush becomes the remainder
+  writeCmd(DCS_RAMWR);
+
+  memset(&trans[0], 0, sizeof(spi_transaction_t));
+/*
+  err = gpio_intr_enable((gpio_num_t)PIN_NUM_TE); assert(err==ESP_OK);
+
+  while(!TE_TriggerFlag)
+  ;
+
+  err = gpio_intr_disable((gpio_num_t)PIN_NUM_TE); assert(err==ESP_OK);
+  TE_TriggerFlag = false;
+*/
+  //sync with TE pin pending...
+  while(dmaBlockCount--){
+    trans[0].length = MAX_DMA_LEN*8;
+    trans[0].user = (void*)1; //SDC pin high for data
+    trans[0].tx_buffer=(uint16_t *)color_p;
+    err = spi_device_transmit(ssd2805, &trans[0]); 
+    ESP_ERROR_CHECK(err);
+    color_p+=(MAX_DMA_LEN/2);
+  }
+
+  if(byteCount2Flush){
+    trans[0].length = byteCount2Flush*8;
+    trans[0].user = (void*)1; //SDC pin high for data
+    trans[0].tx_buffer=(uint16_t *)color_p;
+    err = spi_device_transmit(ssd2805, &trans[0]); 
+    ESP_ERROR_CHECK(err);
+  }
+}
---
+++ littlevgl/lv_drivers/indev/SSD2541.c
@@ -0,0 +1,272 @@
+#include "SSD2541.h"
+
+/**
+ * @note  Initialization map to be sent to SSD2541 on POR
+ */
+const I2C_InitMap SSD2541CfgTable[]=
+{
+		{REG_SW_RESET, 			0x0000},	 //sw reset
+		{REG_NOP,				0x012C},	 //wait for 300ms (0x12C=300), use NO Operation command for a delay
+	    {REG_DRIVE_SENSE_NO, 	0x0505},     //Drive/Sense number set 6 for 1.54" CTP (register value = number -1)
+	    {REG_SENSE_OFFSET, 		0x0002},     //offset for sense line, hardware dependent
+	    {REG_DRIVE_LINE0, 		0x000B},     //Select Drive11 as Tx1 for Drive Line 0
+	    {REG_DRIVE_LINE1, 		0x000C},     //Drive 12 > Drive Line 1
+	    {REG_DRIVE_LINE2, 		0x000D},     //Drive 13 > Drive Line 2
+	    {REG_DRIVE_LINE3, 		0x000E},     //Drive 14 > Drive Line 3
+	    {REG_DRIVE_LINE4, 		0x000F},     //Drive 15 > Drive Line 4
+	    {REG_DRIVE_LINE5, 		0x0010},     //Drive 16 > Drive Line 5
+	    {0xC1, 					0x0002},     //magic number, reserved
+	    {REG_DRIVE_LEVEL,		0x0000},     //Drive Voltage Level, set vout = 5.5V
+	    {REG_INT_TIMING,		0x0609},
+	    {REG_MIN_LEVEL,			0xB664},
+	    {REG_ADC_RANGE_SEL,	0x0004},
+	    {REG_BIAS_RES,			0x0004},
+	    {REG_INTG_CAP,			0x0007},
+	    {0x2a,					0x700F},	//magic number
+		{0x2b, 					0x17DD},	//magic number
+		{0x37,					0x07C4},	//magic number
+	    {0x38,					0x0002},
+	    {0x56,					0x8010},
+	    {0x59,					0xFF31},
+	    {REG_EVENT_MASK,		0xffff},
+	    {REG_IRQ_MSK,			0xFFE3},	//how many fingers to detect? it was 0x0003 from Joe's code
+	    {0x40,					0x10C8},
+	    {0x41,					0x0050},
+	    {0x42,					0x001C},
+	    {0x43,					0x0050},
+	    {0x44,					0x001C},
+	    {0x45,					0x0050},
+	    {0x46,					0x101F},
+	    {REG_ORIENTATION,		0x0003},
+	    {REG_X_SCALING,			0x2800},	//REG_X_SCALING & REG_Y_SCALING are scaling factors for screen resolution 240x240
+	    {REG_Y_SCALING,			0x2800},
+	    {0x8a,					0x0002},
+	    {REG_OP_MODE,			0x0008},
+};
+
+/**
+ * ****************** Local members ******************
+ * ***************************************************
+ */
+static int16_t  tp_last_x = 4095, tp_last_y = 4095;
+volatile bool   isr_tp_flag = false;
+static uint16_t getTouchStatus(void);
+static void hal_gpioSetLevel(uint8_t pin, bool level);
+static void hal_delay(uint32_t ms);
+static void hal_readRegister(uint8_t reg, uint8_t* data, uint32_t cnt);
+static void hal_writeRegister(uint8_t reg, uint16_t val);
+
+/**
+ * @brief Intialization function for SSD2541. Need to call this function before usage.
+ */
+void SSD2541_begin(void)
+{
+    int i2c_master_port = I2C_NUM_0;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = PIN_CTP_SDA;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = PIN_CTP_SCL;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = 100000UL;
+    i2c_param_config(i2c_master_port, &conf);
+    esp_err_t err = i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+    ESP_ERROR_CHECK(err);
+
+    //write initialization table
+    for(uint32_t index=0; index<(sizeof(SSD2541CfgTable)/sizeof(I2C_InitMap)); index++)
+    {
+        if(SSD2541CfgTable[index].command==REG_NOP)
+    		  hal_delay(SSD2541CfgTable[index].val);
+    	  else
+    		  hal_writeRegister(SSD2541CfgTable[index].command, SSD2541CfgTable[index].val);
+    }
+}
+
+/**
+ * @brief Toggle reset input for SSD2541
+ */
+void SSD2541_hwReset(void)
+{
+  hal_gpioSetLevel(PIN_CTP_RST, 0);
+  hal_delay(10);
+  hal_gpioSetLevel(PIN_CTP_RST, 1);
+  hal_delay(10);
+}
+
+
+/**
+ * @brief Return Device ID of SSD2541
+ * @return Device ID as 0x2541
+ */ 
+uint16_t SSD2541_readDeviceId(void)
+{
+    uint8_t device_id[2];
+
+    hal_readRegister(REG_DEVICE_ID, (uint8_t *)&device_id, 2);
+
+    return ((uint16_t)device_id[0] << 8 | device_id[1]);
+}
+
+/**
+ * @brief This function returns the last finger coordinates and the current pressure value.
+ * @param *x, *y, & *pressure are pointers to variables to store position and pressure of the finger
+ * @return  false if no valid touch
+ *          true if a valid finger is detected
+ *          
+ * @note    Example to use<br>
+ *          #include "SSD2541.h"
+ *          int16_t finger_x, finger_y;
+ *          uint8_t pressure;
+ *          void setup(){
+ *            Serial.begin(115200);
+ *            SSD2541_begin();
+ *          }
+ *          void loop() {
+ *            delay(500);            
+ *            SSD2541_getPoint(&finger_x, &finger_y, &pressure);
+ *            //Observe Serial Monitor for printout of the current touch coordinates and pressure
+ *            Serial.printf("Touch x,y,pressure = %d,%d,%d\n", finger_x, finger_y, pressure);
+ *            }
+ */
+bool SSD2541_getPoint(int16_t *x, int16_t *y, uint8_t *pressure)
+{
+	if((x==NULL) || (y==NULL)) return false;
+
+	bool valid = false;
+  
+	touch_t touch;
+	memset(&touch, 0, sizeof(touch_t));
+	int16_t finger_x=0, finger_y=0;
+
+	if((getTouchStatus()&0x1000) == 0x1000) //a single touch is valid
+	{
+		//snippet here is executed once on POR therefore tp_last_x/y are set to 0xfff when no finger is detected
+		hal_readRegister(REG_FINGER00, (uint8_t *)touch.data, 4);
+		finger_x = (int16_t)touch.finger.lo_x + ((int16_t)touch.finger.hi_x<<8);
+		finger_y = (int16_t)touch.finger.lo_y + ((int16_t)touch.finger.hi_y<<8);
+		if((finger_x <= CTP_SIZE_X) | (finger_y <=CTP_SIZE_Y)){	//check if touch values fit the screen size. Sometimes finger_x/y returns 0xfff!
+		tp_last_x = *x = finger_x;
+		tp_last_y = *y = finger_y;
+		if(pressure!=NULL){*pressure = touch.finger.pressure;}	//check if user is passing a NULL to *pressure argument because pressure is optional
+		}
+	valid = true;
+	}else{
+		//when a finger is released this is the place to go...
+		if(pressure!=NULL) {*pressure = 0;}
+		*x = tp_last_x; //this is to suit LittlevGL as the last finger position is required for porting
+		*y = tp_last_y; //it is also possible to return -1 when a finger has been lifted to indicate the current position
+	valid = false;
+	}
+
+	return valid;
+}
+
+/**
+ * @brief Returns Touch Flag (isr_tp_flag)
+ * @return  true if a finger is detected
+ *          false if no finger is detected.
+ */
+bool SSD2541_getTouchFlag(void)
+{ 
+  return isr_tp_flag;
+}
+
+/**
+ * @brief This function disable interrupt first, then clear the Touch Flag (isr_tp_flag), and re-enable interrupt for next touch
+ */
+void SSD2541_clearTouchFlag(void)
+{
+  //ESP32 IDF
+  //...pending
+}
+
+/**
+ * @brief	Touch Status return by reading STATUS REGISTER R79h
+ * @param	None
+ * @return	Register value in 16-bit width with buf[1] shift to higher byte
+ *
+ * 			Register value R79h contains the bits for Finger00 - Finger09 plus few abnormality bits.
+ * 			Example, getTouchStatus() = 0x1000 if Finger00 (the first touch) is detected
+ * 			getTouchStatus() = 0x3000 if Finger01 & Finger00 is detected, i.e. two fingers.
+ * 			If 1st finger is released getTouchStatus() = 0x2000; else,
+ *          if 2nd finger is released getTouchStatus() returns 0x1000.
+ *      When a valid finger is detected the interrupt will be set low from high (H->L edge) until
+ *      this STATUS register is read, the pin will be reset to high by reading REG_TOUCH_STATUS.
+ */
+uint16_t getTouchStatus(void)
+{
+    uint8_t buf[2];
+    hal_readRegister(REG_TOUCH_STATUS, (uint8_t *)&buf, 2);
+    
+    uint16_t sta = (uint16_t)buf[1]<<8 | buf[0];
+  
+    return sta;
+}
+
+/**
+ * @brief HAL function to set/clear GPIO level.
+ *        Should have set the pin as an output in SSD2541_begin().
+ */
+static void hal_gpioSetLevel(uint8_t pin, bool level)
+{
+	gpio_set_level((gpio_num_t)pin, (uint32_t)level);
+}
+
+/**
+ * @brief   HAL delay function for millisec
+ */ 
+void hal_delay(uint32_t ms)
+{
+    vTaskDelay(ms/portTICK_PERIOD_MS);
+}
+
+/**
+ * @brief   HAL I2C read from SSD2541
+ * @param   *data is a pointer to buffer to store bytes returned
+ * @param   cnt is the byte count
+ * @note    Example : 
+ *          uint8_t buffer[2];
+ *          hal_readRegister(REG_DEVICE_ID, (uint8_t *)&buf, 2);
+ *          //buffer[1][0] = [41h][25h]
+ */ 
+void hal_readRegister(uint8_t reg, uint8_t* data, uint32_t cnt)
+{
+	if(cnt==0) return;
+
+	esp_err_t err;
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (SSD2541_I2C_ADDR << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (SSD2541_I2C_ADDR << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+	if(cnt > 1){
+		i2c_master_read(cmd, data, cnt - 1, ACK_VAL);
+	}
+	i2c_master_read_byte(cmd, data + cnt - 1, NACK_VAL);
+	i2c_master_stop(cmd);
+	err = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+  ESP_ERROR_CHECK(err);
+	i2c_cmd_link_delete(cmd);
+}
+
+/**
+ * @brief	HAL I2C write to SSD2541
+ * @param	reg is the register to write to
+ * @param	val is a 16 bit value to write
+ */ 
+void hal_writeRegister(uint8_t reg, uint16_t val)
+{
+    esp_err_t err;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (SSD2541_I2C_ADDR << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, (uint8_t)(val>>8), ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, (uint8_t)val&0xff, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    err = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+    ESP_ERROR_CHECK(err);
+    i2c_cmd_link_delete(cmd);
+}
